5a011c14414dc347cdeaeed05b835e5164dc2ccc
Victor Strandmoe <victorstrandmoe@gmail.com>
2025-11-05 22:26:14 +0100

    simplify badpython

--- sast/python/project/badpython.py
+++ sast/python/project/badpython.py
@@ -2,22 +2,14 @@
 import os
 import hashlib
 
-# 🚨 Hardcoded API key (should trigger p/secrets)
-API_KEY = "cbc68b07-cc5a-4bfb-827b-97c31807c913"
-
 # 🚨 Using eval on user input (code injection risk)
 user_input = input("Enter Python code: ")
 eval(user_input)
 
-# 🚨 Hardcoded password
-DB_PASSWORD = "SuperSecret123!"
 
 # 🚨 Insecure hash (MD5)
 password_hash = hashlib.md5("mypassword".encode()).hexdigest()
 
-# 🚨 Weak crypto usage
-import cryptography
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 key = b"1234567890123456"
 iv = b"0000000000000000"
